61f213e2f78194ec0a8dbc37ee55f5b67abc172c
student <bala01081977@gmail.com>
2023-06-01 19:32:21 +0300

    завдання №5

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-#4
-word = input("Введіть слово: ")
-print("Результат: ")
-for i in word:
-    print(i)
+#5
+def string(s):
+    s = s.lower()
+    s = s.replace(" ", "")
+    if s == s[::-1]:
+        print("Слово - паліндром")
+    else:
+        print("Не є паліндромом")
+s1 = input("Введіть рядок: ")
+string(s1)
