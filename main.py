c1f0932012996c8d7234edfb7d7844ed37060e2c
student <bala01081977@gmail.com>
2023-06-01 19:23:09 +0300

    завдання №2

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-#1
-print("Hello world!")
+#2
+a = int(input("Введіть перше число діапазону: "))
+b = int(input("Введіть друге число діапазону: "))
+summ = 0
+for i in range(a, b+1):
+    summ += i
+print(f"Сума чисел в діапазоні становить: {summ}")
